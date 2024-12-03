5eecd7d3802673fee3c12292e6b7142b1f3a2a10
Sacha <sacha.okus@gmail.com>
2024-12-03 01:28:03 +0000

    Vehicle types added

---
+++ src/Vehicle.java
@@ -0,0 +1,19 @@
+package com.parkinglot;
+
+public abstract class Vehicle {
+    private String licensePlate;
+    private VehicleType type;
+
+    public Vehicle(String licensePlate, VehicleType type) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+}
---
+++ src/VehicleType.java
@@ -0,0 +1,7 @@
+package com.parkinglot;
+
+public enum VehicleType {
+    MOTORCYCLE,
+    CAR,
+    VAN
+}
